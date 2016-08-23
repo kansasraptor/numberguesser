659a33f541f4b7de09bac9f9c556ceb52bd47045
Laura Allen <laurasellers@gmail.com>
2016-08-23 15:45:47 -0600

    added variable for input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 var guess = document.getElementById('#guessBox')
 var submit = document.querySelector('.pickNumber');
 var clearButton = document.querySelector('.clear');
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+var input = document.querySelector('#guessBox')
+var number = 10
 
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+// When a user clicks on "Guess A Number"
+// get value from guessBox
+// I want to store that value in a variable
+// I already have a variable with a umber in it called var number
+// Is user guess bigger than var number?
+  // If yes : display "too high" somewhere on the page (where?!)
+  // If no: display "too low" somewher *where?!)
 
+// get value from input field javascript
 
 submit.addEventListener ('click', function (){
 
